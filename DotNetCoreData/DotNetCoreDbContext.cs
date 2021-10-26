f017b7b1f0d68078970414a5c40143415ea914f5
sarathiidss <sarathi@idsoftsource.com>
2021-10-26 20:11:26 +0530

    Add project files

---
+++ DotNetCoreData/DotNetCoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DotNetCoreEntity.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreData
+{
+    public class DotNetCoreDbContext : DbContext
+    {
+        public DotNetCoreDbContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
