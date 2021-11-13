f383759c1abb860d2cc18398b356db0f6b94f1f4
sarathiidss <sarathi@idsoftsource.com>
2021-11-13 13:03:24 +0530

    Changes

---
+++ ScaffoldDbLib/Class1.cs
@@ -0,0 +1,25 @@
+﻿using ScaffoldDbLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScaffoldDbLib
+{
+    class Class1
+    {
+        public Class1(ScaffoldDbContext context)
+        {
+            context.Users.FromSqlRaw(@"CREATE PROCEDURE TestSp 
+          AS
+            BEGIN
+
+         Select * from dbo.[User];
+            END
+            GO");
+            context.Users.FromSqlRaw("EXEC TestSp");
+        }
+    }
+}
