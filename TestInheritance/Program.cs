ada82878dc3c83252c5565df03db937f722a525c
sarathiidss <sarathi@idsoftsource.com>
2021-11-26 16:08:29 +0530

    Changes

--- TestInheritance/Program.cs
+++ TestInheritance/Program.cs
@@ -12,7 +12,23 @@ namespace TestInheritance
             //demo1.Show();
             //demo1.Hide();
 
+            Student student1 = new Student() { Age = 20 };
+            Student student2 = new Student() { Age = 30 };
+
+            Console.WriteLine(student1 + student2);
+
             Console.ReadLine();
+        }
+    }
+
+    public class Student
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public static int operator +(Student student1, Student student2)
+        {
+            return student1.Age + student2.Age;
         }
     }
 
@@ -21,6 +37,36 @@ namespace TestInheritance
         public virtual void Show()
         {
             Console.WriteLine("Demo 1");
+        }
+
+        public void Show(int a)
+        {
+
+        }
+
+        public void Show(string a)
+        {
+
+        }
+
+        public void Show(int a, string b)
+        {
+
+        }
+
+        public void Show(string a, int b)
+        {
+
+        }
+
+        public void Show(int a, int b)
+        {
+
+        }
+
+        public int Show(bool a, int b)
+        {
+            return b;
         }
     }
 
